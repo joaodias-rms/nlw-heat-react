dc62246cf936d4236f3870830aed2b0657fe43b7
joao <jvd.ramos@hotmail.com>
2021-10-22 10:53:57 -0400

    ajuste para uso de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import styles from './App.module.scss'
-import { MessageList } from './components/MessageList'
-import { LoginBox } from './components/LoginBox'
+import styles from "./App.module.scss";
+import { MessageList } from "./components/MessageList";
+import { LoginBox } from "./components/LoginBox";
+import { useContext } from "react";
+import { AuthContext } from "./context/auth";
+import { SendMessageForm } from "./components/SendMessageForm";
 
 export function App() {
-  return(
-   <main className={styles.contentWrapper}>
-     <MessageList />
-     <LoginBox />
-   </main>
-  )
-}
+  const { user } = useContext(AuthContext);
 
+  return (
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned: ''}`}>
+      <MessageList />
+
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
+  );
+}
