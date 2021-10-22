7fa6ba69418bb12c897ab0126aa6f9b8c90699b0
joao <jvd.ramos@hotmail.com>
2021-10-22 10:55:07 -0400

    criado formulário de mensagens

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,8 +1,54 @@
+import { useState, FormEvent } from "react";
+import { useContext } from "react";
+import { VscSignOut, VscGithubInverted } from "react-icons/vsc";
+import { AuthContext } from "../../context/auth";
+import { api } from "../../services/api";
+import styles from "./styles.module.scss";
 
 export function SendMessageForm() {
-    return (
-        <h1>
-            SendMessageForm
-        </h1>
-    )
+  const { user, signOut } = useContext(AuthContext);
+  const [message, setMessage] = useState('')
+
+  async function handleSendMessage(event:FormEvent){
+      event.preventDefault()
+    if(!message.trim()){
+        return
+    }
+
+    await api.post('message', {message})
+
+    setMessage('')
+  }
+
+
+  return (
+    <div className={styles.sendMessageFormWrapper}>
+      <button className={styles.signOutButton} onClick={signOut}>
+        <VscSignOut size="32" />
+      </button>
+
+      <header className={styles.userInformation}>
+        <div className={styles.userImage}>
+          <img src={user?.avatar_url} alt={user?.name} />
+        </div>
+        <strong className={styles.userName}>{user?.name}</strong>
+        <span className={styles.userGithub}>
+          <VscGithubInverted size="16" />
+          {user?.login}
+        </span>
+      </header>
+
+      <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
+        <label htmlFor="message">Mensagem</label>
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Qual sua expectativa para o evento?"
+          onChange={event => setMessage(event.target.value)}
+          value={message}
+        />
+        <button type="submit">Enviar mensagem</button>
+      </form>
+    </div>
+  );
 }
