605febbb0b1c3f3cfea83a38b944e5c00a54cbdb
joao <jvd.ramos@hotmail.com>
2021-10-22 10:54:51 -0400

    add efeito pra validar a messagelist em realtime

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { api } from "../../services/api";
-
+import io from 'socket.io-client'
 import styles from "./styles.module.scss";
 
 import logoImg from "../../assets/logo.svg";
@@ -14,8 +14,30 @@ type Message = {
   };
 };
 
+const messagesQueue:Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage:Message)=>{
+    messagesQueue.push(newMessage)
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const timer = setInterval(()=>{
+      if(messagesQueue.length > 0){
+        setMessages(prevState=>[
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean))
+
+        messagesQueue.shift()
+      }
+    },2000)
+  }, [])
 
   useEffect(() => {
     api.get<Message[]>("message/last3").then((response) => {
